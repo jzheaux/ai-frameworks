a860de94a0ae0d94847248f5b5ed5404a5046734
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-02-11 11:54:38 -0700

    Adding Manual Feedback
    Feedback can be added to the vector store at runtime
    in order to inform future responses.

--- src/main/java/io/jzheaux/pluralsight/spring/ai_frameworks/FeedbackTools.java
+++ src/main/java/io/jzheaux/pluralsight/spring/ai_frameworks/FeedbackTools.java
@@ -1,8 +1,30 @@
 package io.jzheaux.pluralsight.spring.ai_frameworks;
 
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
+@RestController
 public class FeedbackTools {
-    
+    private final VectorStore vectors;
+
+    FeedbackTools(VectorStore vectors) {
+        this.vectors = vectors;
+    }
+
+    @PostMapping("/feedback")
+    void manualFeedback(@RequestBody String feedback) {
+        addFeedback(feedback);
+    }
+
+    void addFeedback(String feedback) {
+        this.vectors.add(new TokenTextSplitter().transform(List.of(new Document(feedback))));
+    }
 }
